b3efb96ee62d55c25ed0f8daf9498b1773b59c3d
David Cool <david@davidcool.com>
2018-09-10 18:28:51 -0400

    Add files via upload

--- _firmware/robot_firmware_v5.ino
+++ _firmware/robot_firmware_v5.ino
@@ -1,10 +1,15 @@
 // ---------------------------------------------------------------------------
+// Mr. Roboto
 // Robot Firmware
 // v5.0
 // David Cool
 // http://davidcool.com
 //
+// Project archive: https://github.com/davidcool/Mr-Roboto
+//
 // Firmware to control a laser cut robot using servos, pots and a 4 position toggle switch
+//
+// Mr. Roboto can interface with Mr. Touch Key (https://github.com/davidcool/Mr-Touch-Key) over serial
 // ---------------------------------------------------------------------------
 
 #include <Servo.h>                                                  // include the servo library for servo related functions
@@ -12,25 +17,25 @@
 const byte NUMBER_OF_SERVOS = 4;                                    // set to number of servos & pots you'll be connecting
 const byte NUMBER_OF_TOGGLE_POSITIONS = 4;                          // set to number of positions (throws) for switch
                                                                     // arrays are used to store values of each servo independently, make sure arrays size matches num of servos & throws
-const int togglePin[NUMBER_OF_TOGGLE_POSITIONS] = {2,3,4,5};        // the digital pin number for the SPDT toggle switch (connect center pin/pole to GND, throws to pins 2, 3, 4, 5)
-const int servoPin[NUMBER_OF_SERVOS] = {6,7,8,9};                   // the digital pin number for the servo signal
-const int potPin[NUMBER_OF_SERVOS] = {0,1,2,3};                     // the analog pin number for the pot signal (10K potentiometers)
+const int togglePin[NUMBER_OF_TOGGLE_POSITIONS] = {2,3,4,5};        // the digital pin number for the toggle switch (connect center pin/pole to GND, throws to pins 2, 3, 4, 5)
+const int servoPin[NUMBER_OF_SERVOS] = {6,7,8,9};                   // the digital pin numbers for the servo pulse connections
+const int potPin[NUMBER_OF_SERVOS] = {0,1,2,3};                     // the analog pin number for the pot signals (10K potentiometers)
 
-const int toggleInterval[NUMBER_OF_TOGGLE_POSITIONS] = {200,200,200,200};  // number of millisecs between toggle readings
+const int toggleInterval[NUMBER_OF_TOGGLE_POSITIONS] = {150,150,150,150};  // number of millisecs between toggle readings
 const int servoMinDegrees[NUMBER_OF_SERVOS] = {0,0,0,0};            // the limits to servo movement (0-180 nominal values)
-const int servoMaxDegrees[NUMBER_OF_SERVOS] = {178,178,178,178};    // calibrate the values to your servos
+const int servoMaxDegrees[NUMBER_OF_SERVOS] = {180,180,180,180};    // calibrate the values to your servos
 
 Servo myservo[NUMBER_OF_SERVOS];                                    // create servo object to control a servo 
 
-bool toggleState[NUMBER_OF_TOGGLE_POSITIONS] = {HIGH,HIGH,HIGH,HIGH};    // the current state of each toggle pin
+bool toggleState[NUMBER_OF_TOGGLE_POSITIONS] = {HIGH,HIGH,HIGH,HIGH};    // set default toggle pins to high. When toggle position is selected it will go LOW b/c pole is connected to GND
 
 int serialRead[2];                                                  // serial data from keyboard containing current pitch and note on/off value
 
-int servoInterval[NUMBER_OF_SERVOS] = {15,15,15,15};                // initial millisecs between servo reads
+int servoInterval[NUMBER_OF_SERVOS] = {100,100,100,100};                // initial millisecs between servo reads
 int servoPosition[NUMBER_OF_SERVOS] = {90,90,90,90};                // the current angle of the servo - starting at 90 degrees
-int servoDegrees[NUMBER_OF_SERVOS] = {3,3,3,3};                     // amount servo moves at each step, negative value moves opposite direction
+int servoDegrees[NUMBER_OF_SERVOS] = {1,1,1,1};                     // amount servo moves at each step, negative value moves opposite direction
 
-int potInterval[NUMBER_OF_SERVOS] = {15,15,15,15};                  // initial millisecs between pot reads
+int potInterval[NUMBER_OF_SERVOS] = {100,100,100,100};                  // initial millisecs between pot reads
 int potVal[NUMBER_OF_SERVOS] = {0,0,0,0};                           // stores value of pot position, zero to initialize
 int potValKnob[NUMBER_OF_SERVOS] = {0,0,0,0};                       // stores value of pot position, zero to initialize
 int potValSweep[NUMBER_OF_SERVOS] = {0,0,0,0};                      // stores value of pot position, zero to initialize
@@ -43,10 +48,10 @@ unsigned long previousPotMillis[NUMBER_OF_SERVOS] = {0,0,0,0};      // time when
 unsigned long previousServoMillis[NUMBER_OF_SERVOS] = {0,0,0,0};    // the time when the servo was last checked, zero to initialize
 unsigned long servoRandomMillis[NUMBER_OF_SERVOS] = {0,0,0,0};      // the random time periods between servo direction changes, zero to initialize
 
-int led = 13;
+int led = 13;                                                       // Use pin 13 for led feedback
 bool count = 0;
 
-int pitch[] = {36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47};
+int pitch[] = {36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47};     // array holds MIDI note pitch values starting at C3
 
 void setup() {
   
@@ -56,21 +61,21 @@ void setup() {
  for (int i=0; i <NUMBER_OF_SERVOS; i++) {                          // iterate through each servo in array based on total servos number
    myservo[i].write(servoPosition[i]);                              // sets the initial position
    myservo[i].attach(servoPin[i]);                                  // connects each servo to proper pin
-   servoRandomMillis[i] = random(2000,20000);                          // pick a random time period between min & max number
+   servoRandomMillis[i] = random(4000,20000);                       // pick a random time period between min & max number
  }
  for (int i=0; i <NUMBER_OF_TOGGLE_POSITIONS; i++) {
   pinMode(togglePin[i], INPUT_PULLUP);                              // configure toggle pins as inputs and enable the internal pull-up resistors
  }                                                                  // when toggle pin is engaged the signal goes LOW b/c the pole is connected to GND
   // initialize the digital pin as an output.
-  pinMode(led, OUTPUT);
+  pinMode(led, OUTPUT);                                             // led is set to output and will show serial traffic
 
 }
 
 void loop() {
 
-if (Serial) {
+if (Serial1) {
   count = 1;
-} else if ( (!Serial) && (count == 1) ) {
+} else if ( (!Serial1) && (count == 1) ) {
   asm volatile ("  jmp 0");
 }
 if (Serial1.available() >= 2){
@@ -90,8 +95,8 @@ if (Serial1.available() >= 2){
  
  servoKnob(0);                                                       // run manual knob control function if proper toggle position is set to high
  servoSweep(1);                                                      // run auto sweep function if proper toggle position is set to high
- //servoRandomSweep(3);                                                // run auto random sweep function if proper toggle position is set to high
- servoRandomSweepMIDI(2);
+ //servoRandomSweep(2);                                                // run auto random sweep function if proper toggle position is set to high
+ servoRandomSweepMIDI(2);                                            // each time a random direction change happens in servo, it sends a random MIDI note to Mr. Touch Key over serial
  servoMusicDance(3);                                                 // robot reacts to touch piano via serial data
 
 }
@@ -123,9 +128,9 @@ void readPots() {
   for (int i=0; i <NUMBER_OF_SERVOS; i++) {
      if (currentMillis - previousPotMillis[i] >= potInterval[i]) {
         potVal[i] = analogRead(potPin[i]);                           // reads the value of the potentiometer (value between 0 and 1023)
-        potValKnob[i] = map(potVal[i], 0, 1023, 5, 170);             // scale it to use it with the knob (position between 0-180 deg nominal)
+        potValKnob[i] = map(potVal[i], 0, 1023, 0, 180);             // scale it to use it with the knob (position between 0-180 deg nominal)
         potValSweep[i] = map(potVal[i], 0, 1023, 5, 50);             // scale it to use it with the servo (slow to fast millisecs speed)
-        potValMIDI[i] = map(potVal[i], 0, 1023, 1000, 5000);
+        potValMIDI[i] = map(potVal[i], 0, 1023, 500, 5000);
         servoInterval[i] = potValSweep[i];                           // set millisec speed for servo interval based on pot value
         previousPotMillis[i] += potInterval[i];                      // record time when pot changed value
      }
@@ -168,7 +173,7 @@ void servoRandomSweep(int toggleNum) {
       if (currentMillis - previousServoMillis[i] >= servoInterval[i]) {
        if (currentMillis > servoRandomMillis[i]) {                    // is it time to trigger a random direction change of the servo?
         servoDegrees[i] = - servoDegrees[i];                          // reverse direction
-        servoRandomMillis[i] = random(0,10000) + currentMillis;       // pick new random time and add to current time for future trigger     
+        servoRandomMillis[i] = random(500,10000) + currentMillis;       // pick new random time and add to current time for future trigger     
        }
        servoPosition[i] = servoPosition[i] + servoDegrees[i];         // servoDegrees might be negative  
        if ((servoPosition[i] >= servoMaxDegrees[i]) || (servoPosition[i] <= servoMinDegrees[i]))  { // if the servo is at either extreme change the sign of the degrees to make it move the other way           
@@ -251,7 +256,7 @@ void servoMusicDance(int toggleNum){
           if ( (serialRead[1] == 36 || serialRead[1] == 37 || serialRead[1] == 38) && (i == 0) ) {
             if (currentMillis > servoRandomMillis[i]) {                    // is it time to trigger a random direction change of the servo?
             servoDegrees[i] = - servoDegrees[i];                          // reverse direction
-            servoRandomMillis[i] = random(0,10000) + currentMillis;       // pick new random time and add to current time for future trigger     
+            servoRandomMillis[i] = random(500,10000) + currentMillis;       // pick new random time and add to current time for future trigger     
            }
            
            servoPosition[i] = servoPosition[i] + servoDegrees[i];         // servoDegrees might be negative  
@@ -268,7 +273,7 @@ void servoMusicDance(int toggleNum){
           if ( (serialRead[1] == 39 || serialRead[1] == 40 || serialRead[1] == 41) && (i == 1) ) {
             if (currentMillis > servoRandomMillis[i]) {                    // is it time to trigger a random direction change of the servo?
             servoDegrees[i] = - servoDegrees[i];                          // reverse direction
-            servoRandomMillis[i] = random(0,10000) + currentMillis;       // pick new random time and add to current time for future trigger     
+            servoRandomMillis[i] = random(500,10000) + currentMillis;       // pick new random time and add to current time for future trigger     
            }
            
            servoPosition[i] = servoPosition[i] + servoDegrees[i];         // servoDegrees might be negative  
@@ -285,7 +290,7 @@ void servoMusicDance(int toggleNum){
           if ( (serialRead[1] == 42 || serialRead[1] == 43 || serialRead[1] == 44) && (i == 2) ) {
             if (currentMillis > servoRandomMillis[i]) {                    // is it time to trigger a random direction change of the servo?
             servoDegrees[i] = - servoDegrees[i];                          // reverse direction
-            servoRandomMillis[i] = random(0,10000) + currentMillis;       // pick new random time and add to current time for future trigger     
+            servoRandomMillis[i] = random(500,10000) + currentMillis;       // pick new random time and add to current time for future trigger     
            }
            
            servoPosition[i] = servoPosition[i] + servoDegrees[i];         // servoDegrees might be negative  
@@ -302,7 +307,7 @@ void servoMusicDance(int toggleNum){
           if ( (serialRead[1] == 45 || serialRead[1] == 46 || serialRead[1] == 47) && (i == 3) ) {
             if (currentMillis > servoRandomMillis[i]) {                    // is it time to trigger a random direction change of the servo?
             servoDegrees[i] = - servoDegrees[i];                          // reverse direction
-            servoRandomMillis[i] = random(0,10000) + currentMillis;       // pick new random time and add to current time for future trigger     
+            servoRandomMillis[i] = random(500,10000) + currentMillis;       // pick new random time and add to current time for future trigger     
            }
            
            servoPosition[i] = servoPosition[i] + servoDegrees[i];         // servoDegrees might be negative  
